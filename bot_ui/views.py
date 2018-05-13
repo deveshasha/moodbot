0e0ced9e99e7dda656808a8762fa64389ee355ae
Devesh <deveshasha.2@gmail.com>
2018-05-13 23:31:57 +0530

    dfhj

--- bot_ui/views.py
+++ bot_ui/views.py
@@ -1,9 +1,18 @@
+import subprocess
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from subprocess import Popen, PIPE
+
 
 def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'bot_ui/index.html')
-	# template = loader.get_template('bot_ui/index.html')
-	# return HttpResponse("Hello, world. You're at the polls index.")
+	command = 'python -m rasa_core.run -d rasa/models/dialogue -u rasa/models/nlu/default/current'
+	result = subprocess.run(command, shell=True)
+	# #output = result.stdout.decode("utf-8")
+	# #output = "\n".join(output.splitlines())
+	# output=''
+	for line in result.stdout:
+		print(line.decode(), end='')
+	return render(request, 'bot_ui/index.html')
