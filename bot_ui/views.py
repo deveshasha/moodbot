078984efcf3773d85912cc5e0168f79e6218e831
Devesh <deveshasha.2@gmail.com>
2018-05-13 16:49:49 +0530

    added css styling

--- bot_ui/views.py
+++ bot_ui/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    #return render(request, 'bot_ui/index.html')
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'bot_ui/index.html')
 	# template = loader.get_template('bot_ui/index.html')
 	# return HttpResponse("Hello, world. You're at the polls index.")
--- moodbot/settings.py
+++ moodbot/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/2.0/howto/static-files
+#STATIC_ROOT = ''
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
