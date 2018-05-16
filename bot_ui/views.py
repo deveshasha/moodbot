7599b9c3dd8200253d1f0fd7a24841e620e108da
Devesh <deveshasha.2@gmail.com>
2018-05-17 00:47:56 +0530

    html change

--- bot_ui/views.py
+++ bot_ui/views.py
@@ -24,16 +24,23 @@ def get_input(request):
 		form = ChatForm(request.POST)
 		if form.is_valid():
 			user_input = request.POST.get('input_text')
-
-			html_ = '''<div class="chat-message clearfix">
-						<div class="chat-message-content clearfix">	<span class="chat-time">13:37</span>
-							<h5> You </h5>
-							<p> ''' + user_input +''' </p>
-						</div>
-						<!-- end chat-message-content -->
-					</div>'''
-			return render(request, 'bot_ui/index.html', {'html_':html_})
+			all_inputs = chatLogger(user_input)
+			
+			# html_ = '''<div class="chat-message clearfix">
+			# 			<div class="chat-message-content clearfix">	<span class="chat-time">13:37</span>
+			# 				<h5> You </h5>
+			# 				<p> ''' + user_input +''' </p>
+			# 			</div>
+			# 			<!-- end chat-message-content -->
+			# 		</div>'''
+			return render(request, 'bot_ui/index.html', {'all_inputs':all_inputs})
 	else:
 		form = ChatForm()
 
 	return render(request, 'bot_ui/index.html', {'form':form})
+
+def chatLogger(input):
+	chat_history = []
+	chat_history.append(input)
+	return chat_history
+	
